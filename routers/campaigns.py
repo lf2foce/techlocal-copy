683cb14c7da30c2de2126af91a420a583ec2758b
Anh Bui <lf2foce@gmail.com>
2025-04-04 11:11:30 +0700

    nothing change

--- routers/campaigns.py
+++ routers/campaigns.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
+from fastapi import status
+
 from sqlalchemy.orm import Session
 from database.db import get_db
 from database.models import Campaign
@@ -14,7 +16,7 @@ def list_campaigns(db: Session = Depends(get_db)):
 
 @router.post("/", response_model=CampaignResponse)
 def create_campaign(payload: CampaignCreate, db: Session = Depends(get_db)):
-    new_campaign = Campaign(**payload.dict())
+    new_campaign = Campaign(**payload.model_dump())
     db.add(new_campaign)
     db.commit()
     db.refresh(new_campaign)
@@ -26,3 +28,16 @@ def get_campaign(campaign_id: int, db: Session = Depends(get_db)):
     if not campaign:
         raise HTTPException(status_code=404, detail="Campaign not found")
     return campaign
+
+@router.delete("/{campaign_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_campaign(campaign_id: int, db: Session = Depends(get_db)):
+    campaign = db.query(Campaign).filter(Campaign.id == campaign_id).first()
+    if not campaign:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Campaign not found"
+        )
+    
+    db.delete(campaign)
+    db.commit()
+    return None
